ff2057fb78d9a759c393691f252d6d2d3d3def37
chenlei <791563973@qq.com>
2023-11-01 21:12:07 +0800

    feat: 引入 ant design vue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 import "./style.css";
 import App from "./App.vue";
 import { router } from "@/router/index.ts";
 
 const app = createApp(App);
 app.use(router);
+app.use(Antd);
 app.mount("#app");
