ab930fd1708d78ff6335f2699d7f4e6415695a8e
chenlei <791563973@qq.com>
2023-11-08 21:11:41 +0800

    feat: 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8080,
+    port: 8080
   },
   resolve: {
     alias: {
-      "@": "/src",
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
-  plugins: [vue()],
+  plugins: [vue()]
 });
