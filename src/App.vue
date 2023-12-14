db682dee5567d7bf719505bd73a6ae6bbd394275
chenlei <791563973@qq.com>
2023-12-14 22:44:27 +0800

    feat: 默认中文

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
+<template>
+  <a-config-provider :theme="theme" :locale="zhCN">
+    <RouterView></RouterView>
+  </a-config-provider>
+</template>
+
 <script setup lang="ts">
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+
 const theme = {
   token: {
     borderRadius: 2
   }
 };
 </script>
-
-<template>
-  <a-config-provider :theme="theme">
-    <RouterView></RouterView>
-  </a-config-provider>
-</template>
 
 <style scoped>
 .logo {
