55e1f9dbed252aa43543537f20f1f7954553da2e
chenlei <791563973@qq.com>
2023-12-14 22:19:16 +0800

    feat: 指令页面

--- src/views/directive/Index.vue
+++ src/views/directive/Index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>指令</div>
+  <div>
+    <a-row :gutter="16">
+      <a-col :span="8">
+        <a-card title="复制指令" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="8">
+        <a-card title="Loading 指令" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="8">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
