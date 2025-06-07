5b965661bab82d806f0ba0f08d79f2e4cc067249
chenlei <791563973@qq.com>
2025-06-07 12:50:40 +0800

    feat: 添加用户表单

--- src/views/system/user/components/Add.vue
+++ src/views/system/user/components/Add.vue
@@ -1,7 +1,16 @@
 <template>
-  <a-modal v-model:open="open" title="添加用户">
+  <a-modal v-model:open="open" title="添加用户" @ok="handleOk" @cancel="handleCancel">
     <div class="my-[20px]">
-      <UserForm v-model:form-state="formState"></UserForm>
+      <a-form
+        ref="formRef"
+        :model="formState"
+        :label-col="{ style: { width: '70px' } }"
+        autocomplete="off"
+        @ok="handleOk"
+        @cancel="handleCancel"
+      >
+        <UserForm v-model:form-state="formState"></UserForm>
+      </a-form>
     </div>
   </a-modal>
 </template>
@@ -10,13 +19,18 @@
 import { ref, reactive } from "vue";
 import UserForm from "./UserForm.vue";
 
-const open = ref(false);
 const formState = reactive({
+  phone: "",
   username: "",
-  account: "",
   status: 1,
 });
+const formRef = ref();
+async function handleOk() {
+  await formRef.value.validate();
+}
+function handleCancel() {}
 
+const open = ref(false);
 function showModal() {
   open.value = true;
 }
--- src/views/system/user/components/UserForm.vue
+++ src/views/system/user/components/UserForm.vue
@@ -1,31 +1,18 @@
 <template>
-  <a-form
-    layout="vertical"
-    :modal="formState"
-    :label-col="{ style: { width: '80px' } }"
-    :rules="rules"
-  >
-    <a-form-item label="用户名" name="username">
-      <a-input v-model:value="formState.username"></a-input>
-    </a-form-item>
-    <a-form-item label="账号" name="account">
-      <a-input v-model:value="formState.account"></a-input>
-    </a-form-item>
-    <a-form-item label="状态" name="state">
-      <a-radio-group v-model:value="formState.status" button-style="solid">
-        <a-radio-button :value="1">启用</a-radio-button>
-        <a-radio-button :value="0">禁用</a-radio-button>
-      </a-radio-group>
-    </a-form-item>
-  </a-form>
+  <a-form-item label="手机号" name="phone" :rules="[{ required: true }]">
+    <a-input v-model:value="formState.phone"></a-input>
+  </a-form-item>
+  <a-form-item label="用户名" name="username" :rules="[{ required: true }]">
+    <a-input v-model:value="formState.username"></a-input>
+  </a-form-item>
+  <a-form-item label="状态" name="state" :rules="[{ required: true }]">
+    <a-radio-group v-model:value="formState.status" button-style="solid">
+      <a-radio-button :value="1">启用</a-radio-button>
+      <a-radio-button :value="0">禁用</a-radio-button>
+    </a-radio-group>
+  </a-form-item>
 </template>
 
 <script setup lang="ts">
-const rules = {
-  username: [{ required: true, message: "请输入用户名" }],
-  account: [{ required: true, message: "请输入账号" }],
-  status: [{ required: true }],
-};
-
 const formState = defineModel<any>("formState");
 </script>
